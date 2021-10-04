e3ab2ac2e1206bc00eb6d91f4bd5070d22a5d916
K.Kiyokawa <koichi20110068@gmail.com>
2021-10-04 21:40:23 +0900

    example

---
+++ docs/src/pages/example.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { DataTable } from "carbon-components-svelte";
+
+  const rows = [
+    { name: "user1", age: 10, gender: "male" },
+    { name: "user2", age: 20, gender: "female" },
+  ];
+</script>
+
+<DataTable
+  rows="{rows}"
+  headers="{[
+    { key: 'name', value: 'Name' },
+    { key: 'age', value: 'Age' },
+    { key: 'gender', value: 'Gender' },
+  ]}"
+>
+  <div slot="cell" let:cell let:row>
+    {#if cell.key === "age"}{row.age}(years old){:else}{cell.value}{/if}
+  </div>
+</DataTable>
